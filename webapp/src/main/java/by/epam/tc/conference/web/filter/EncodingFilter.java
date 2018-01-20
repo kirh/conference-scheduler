b2b8c87c421d1d9dab0983926315e6f3c30398ff
kirh <hilman@tut.by>
2018-01-20 17:59:42 +0300

    ref

--- webapp/src/main/java/by/epam/tc/conference/web/filter/EncodingFilter.java
+++ webapp/src/main/java/by/epam/tc/conference/web/filter/EncodingFilter.java
@@ -13,7 +13,6 @@ public class EncodingFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-
         request.setCharacterEncoding("UTF-8");
         chain.doFilter(request, response);
     }
