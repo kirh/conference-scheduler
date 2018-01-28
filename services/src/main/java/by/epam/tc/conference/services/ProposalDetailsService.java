42be6ebc7e7ce6ade47a58df2efe9b2d6e5eadd3
kirh <hilman@tut.by>
2018-01-28 16:37:19 +0300

    extracted proposalDetailsService

---
+++ services/src/main/java/by/epam/tc/conference/services/ProposalDetailsService.java
@@ -0,0 +1,25 @@
+package by.epam.tc.conference.services;
+
+import by.epam.tc.conference.dto.ProposalDetails;
+import by.epam.tc.conference.services.exception.ServiceException;
+
+import java.util.List;
+
+public interface ProposalDetailsService {
+
+    /**
+     * Returns list of details for proposals related to section with specified id
+     * @param sectionId section identifier
+     * @return list of proposal details
+     * @throws ServiceException when error during data access occurs
+     */
+    List<ProposalDetails> findProposalsDetailsBySectionId(long sectionId) throws ServiceException;
+
+    /**
+     * Returns list of details for proposals created by participant with specified id
+     * @param participantId participant identifier
+     * @return list of proposal details
+     * @throws ServiceException when error during data access occurs
+     */
+    List<ProposalDetails> findProposalsDetailsByParticipantId(long participantId) throws ServiceException;
+}
