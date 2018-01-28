c0b2e4f5497eb2f1e7a13329d618f398eb7f0fe2
kirh <hilman@tut.by>
2018-01-28 16:17:54 +0300

    added errors constant

---
+++ webapp/src/main/java/by/epam/tc/conference/web/controller/ErrorMessage.java
@@ -0,0 +1,9 @@
+package by.epam.tc.conference.web.controller;
+
+public final class ErrorMessage {
+    public static final String FORBIDDEN = "error.forbidden";
+    public static final String INTERNAL = "error.internal";
+    public static final String NOT_FOUND = "error.not-found";
+    public static final String BAD_REQUEST = "error.bad-request";
+    public static final String USER_ALREADY_EXISTS = "register.error.already-exists";
+}
