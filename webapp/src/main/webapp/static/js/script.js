ee1c056993fd3b7b9094723f55e888eb68b77572
kirh <hilman@tut.by>
2018-01-30 10:52:12 +0300

    removed trash

--- webapp/src/main/webapp/static/js/script.js
+++ webapp/src/main/webapp/static/js/script.js
@@ -12,15 +12,6 @@ function checkpassword() {
     }
 }
 
-// function openEdit() {
-//     var modal = document.getElementById("edit-conference");
-//     modal.style.display = "block";
-//     modal.addEventListener("click", function(event) {
-//         if (event.target == modal) {
-//             modal.style.display = "none";
-//         }
-//     })
-// }
 function validateConferenceDate(input) {
     var dateString = input.value;
     var date = Date.parse(dateString);
@@ -30,43 +21,6 @@ function validateConferenceDate(input) {
         input.setCustomValidity("");
     }
 }
-
-function calculateAge(birthday) {
-    var ageDifMs = Date.now() - birthday.getTime();
-    var ageDate = new Date(ageDifMs);
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
-}
-
-function validateBirthday(birthdayInput) {
-    var birthdayString = birthdayInput.value;
-    var birthday = new Date(birthdayString);
-    var age = calculateAge(birthday);
-    console.log('age = ' + age);
-    if (age < 7 || age > 120) {
-        birthdayInput.setCustomValidity("Age must be between 7 and 120 years")
-    } else {
-        birthdayInput.setCustomValidity("");
-    }
-}
-
-function addAdittionalEmail() {
-    var emailBlock = document.createElement("div");
-    emailBlock.innerHTML = '<input class="form-control" type="email" name="email[]" placeholder="additional email"' +
-        ' required><button class="btn btn-cancel">-</button>';
-
-    var button =  emailBlock.getElementsByTagName('button')[0];
-    button.addEventListener('click', removeAdditionalEmail);
-    emailBlock.appendChild(button);
-    document.getElementById('emails').appendChild(emailBlock);
-}
-
-function removeAdditionalEmail(event) {
-    var emailBlock = event.target.parentNode;
-    var emailsContainer = document.getElementById('emails');
-    emailsContainer.removeChild(emailBlock);
-}
-
-
 
 function confirmAndRedirect(uri) {
     var sure = confirm("Are you sure want to remove this conference?");
