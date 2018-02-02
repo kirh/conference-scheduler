8a2b64fee1208bcc8b732ee00e61c0b0b50bf115
kirh <hilman@tut.by>
2018-02-02 04:34:21 +0300

    conferenceException javadoc

--- commons/src/main/java/by/epam/tc/conference/commons/ConferenceException.java
+++ commons/src/main/java/by/epam/tc/conference/commons/ConferenceException.java
@@ -1,5 +1,8 @@
 package by.epam.tc.conference.commons;
 
+/**
+ * General exception for conference-scheduler application
+ */
 public class ConferenceException extends Exception {
 
     private static final long serialVersionUID = 42L;
