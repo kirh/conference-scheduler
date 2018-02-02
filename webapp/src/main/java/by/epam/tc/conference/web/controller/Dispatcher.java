9772ba82a2bbdaa466a1fd8506ed51e2cb7bbbce
kirh <hilman@tut.by>
2018-02-02 07:11:45 +0300

    javadoc

--- webapp/src/main/java/by/epam/tc/conference/web/controller/Dispatcher.java
+++ webapp/src/main/java/by/epam/tc/conference/web/controller/Dispatcher.java
@@ -6,14 +6,34 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ *Based on incoming query it can redirect, forward to another page or render specified view.
+ *For more information see {@link #dispatch} method
+ */
 public class Dispatcher {
 
-    private static final String REDIRECT_PREFIX = "redirect:";
-    private static final String FORWARD_PREFIX = "forward:";
+    public static final String REDIRECT_PREFIX = "redirect:";
+    public static final String FORWARD_PREFIX = "forward:";
     private static final String VIEW_PATH_PREFIX = "/WEB-INF/jsp/";
     private static final String VIEW_PATH_POSTFIX = ".jsp";
     private static final String CONTROLLER_PREFIX = "/controller";
 
+    /**
+     * Based on incoming query it can redirect, forward to another page or render specified view.<br>
+     * Use in your query: <br>
+     * {@link #REDIRECT_PREFIX} to redirect<br>
+     * {@link #FORWARD_PREFIX} to forward<br>
+     * If prefix is followed by '/' dispatcher interprets it as relative to the servlet container root, otherwise
+     * relative to current uri.
+     * When prefix not specified then it will render view.
+     *
+     * @param query to proceed
+     * @param request object that contains the request the client has made of the servlet
+     * @param response object that contains the response the servlet sends to the client
+     * @throws IOException if the target resource throws this exception
+     * @throws ServletException if the target resource throws this exception
+     * @throws IllegalArgumentException  if the response was already committed
+     */
     public void dispatch(String query, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         if (query.startsWith(REDIRECT_PREFIX)) {
             redirect(query, request, response);
@@ -27,7 +47,7 @@ public class Dispatcher {
         }
     }
 
-    private void redirect(String query,HttpServletRequest request, HttpServletResponse response) throws IOException {
+    private void redirect(String query, HttpServletRequest request, HttpServletResponse response) throws IOException {
         String path = query.replace(REDIRECT_PREFIX, "");
         if (!path.isEmpty()) {
             response.sendRedirect(path);
