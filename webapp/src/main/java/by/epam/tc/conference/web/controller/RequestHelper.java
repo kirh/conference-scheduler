ae241d8af45321d825ed1ac7e95f860bc199ab81
kirh <hilman@tut.by>
2018-02-04 08:00:02 +0300

    javadoc

--- webapp/src/main/java/by/epam/tc/conference/web/controller/RequestHelper.java
+++ webapp/src/main/java/by/epam/tc/conference/web/controller/RequestHelper.java
@@ -1,18 +1,28 @@
 package by.epam.tc.conference.web.controller;
 
 import by.epam.tc.conference.web.controller.command.Command;
+import by.epam.tc.conference.web.controller.command.CommandException;
 import by.epam.tc.conference.web.controller.command.CommandFactory;
 
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * Returns command appropriate for request
+ */
 public class RequestHelper {
     private final CommandFactory commandFactory = CommandFactory.getInstance();
 
-    public Command getCommand(HttpServletRequest request) {
+    /**
+     * Returns command appropriate for request path and parameters
+     * @param request to process
+     * @return command appropriate for request
+     * @throws CommandException when command not found
+     */
+    public Command getCommand(HttpServletRequest request) throws CommandException {
         String path = request.getServletPath();
         String action = request.getParameter("action");
         String pathWithoutLeadingSlash = path.substring(1);
-        String command = action == null ? pathWithoutLeadingSlash : pathWithoutLeadingSlash + "?" + action;
+        String command = action == null ? pathWithoutLeadingSlash : pathWithoutLeadingSlash + "?action=" + action;
         return commandFactory.getCommand(command);
     }
 }
